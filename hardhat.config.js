a9a7ab8f1436cf2b60b0f396192230ceafb25acb
jpage <jpage@interos.ai>
2021-12-13 16:36:42 +0000

    remove private key

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     rinkeby: {
       url: 'https://eth-rinkeby.alchemyapi.io/v2/dKq6V6na9Wwmv1bVpXWKLmWfv-iOX5LP',
-      accounts: ['3761d2fda4b34e16a35b2595638f61941ce93072c77e1bf8299e169a78e8c2a5'],
+      accounts: ['private-key'],
     },
   },
 };
